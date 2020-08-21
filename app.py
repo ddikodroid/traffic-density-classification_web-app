b32545d4314824ee8947316e0ea3724a7c8728b7
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-08-21 13:15:05 +0700

    remove unnecessary code

--- app.py
+++ app.py
@@ -24,34 +24,10 @@ app = Flask(__name__)
 base_path = os.path.dirname(__file__)
 app.config['SECRET_KEY'] = 'ahmadsyarifuddinrandiko'
 app.config['UPLOAD_PATH'] = os.path.join(base_path, 'uploads')
-# app.config['UPLOAD_PATH'] = os.path.join(basedir, 'uploads') # you'll need to create a folder named uploads
+
 
 model_path = 'model/lbp-model.h5'
 # model = load_model(model_path)
-
-# def model_predict(img_path, model):
-#     img = image.load_img(img_path, target_size=(287,304))
-#     x = image.img_to_array(img)
-#     x = np.expand_dims(x, axis=0)
-#     x = preprocess_input(x)
-#     preds = model.predict(x)
-#     return preds
-
-# def decode_predictions(preds, top=4, class_list_path='model/class.json'):
-#   if len(preds.shape) != 2 or preds.shape[1] != 6:
-#     raise ValueError('`decode_predictions` expects '
-#                      'a batch of predictions '
-#                      '(i.e. a 2D array of shape (samples, 6)). '
-#                      'Found array with shape: ' + str(preds.shape))
-#   index_list = json.load(open(class_list_path))
-#   results = []
-#   for pred in preds:
-#     top_indices = pred.argsort()[-top:][::-1]
-#     result = [tuple(index_list[str(i)]) + (pred[i],) for i in top_indices]
-#     result.sort(key=lambda x: x[2], reverse=True)
-#     results.append(result)
-#   print(pred)
-#   return results
 
 @app.route('/', methods=['GET'])
 def index():

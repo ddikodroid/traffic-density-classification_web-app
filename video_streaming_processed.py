a53bbc9075a1f674d26d1e4e1b3921c04b1c6002
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-08-22 04:06:13 +0700

    add lbp but still not working

---
+++ video_streaming_processed.py
@@ -0,0 +1,20 @@
+import cv2
+from preprocessor import *
+from lbp_processor import *
+
+url = 'http://cctv-dishub.sukoharjokab.go.id/zm/cgi-bin/nph-zms?mode=jpeg&monitor=8&scale=150&maxfps=15&buffer=1000&user=user&pass=user'
+
+camera = cv2.VideoCapture(url)
+
+def gen_frames_processed():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            frame_masking(frame)
+            lbp(frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')

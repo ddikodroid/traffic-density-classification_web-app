05f7668c574e527af930eceadf206a2906d13b02
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-08-24 00:15:49 +0700

    remove unnecessary code

--- video_streamer.py
+++ video_streamer.py
@@ -49,18 +49,3 @@ def gen_frames_processed():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        # else:
-        #     frame = frame_masking(frame)
-        #     frame = cropping(frame)
-        #     frame = lbp(frame)
-        #     # print(frame.shape)
-        #     # preds = model.predict(frame)
-        #     # preds = decode_predictions(preds, top=1)
-        #     # preds = str(preds[0,0,1])
-        #     # frame = cv2.putText(frame, 'OpenCV', (50, 50),
-        #     #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0),
-        #     #             2, cv2.LINE_AA)
-        #     ret, buffer = cv2.imencode('.jpg', frame)
-        #     frame = buffer.tobytes()
-        #     yield (b'--frame\r\n'
-        #             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')

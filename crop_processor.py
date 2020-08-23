4f7367cc3fb03512c98f6f2e3836cdca2e0122d8
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-08-23 15:53:21 +0700

    remove unnecessary file, some sample images

--- crop_processor.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-def crop_process(frame):
-    frame = frame[236:540, 280:567]
-    return frame
--- preprocessor.py
+++ preprocessor.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+from skimage.feature import local_binary_pattern
+from PIL import Image
+import matplotlib.pyplot as plt
 
 pts1 = np.array([[0,0],[680,0],[278,540],[0,540]])
 pts2 = np.array([[720,0],[575,0],[560,540],[720,540]])
@@ -9,4 +12,13 @@ def frame_masking(frame):
     cv2.fillPoly(frame,[pts1],(0,0,0))
     cv2.fillPoly(frame,[pts2],(0,0,0))
     cv2.fillPoly(frame,[pts3],(0,0,0))
+    return frame
+
+def lbp(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = local_binary_pattern(frame, 24, 3, method='default')
+    return frame
+
+def cropping(frame):
+    frame = frame[236:540, 280:567]
     return frame

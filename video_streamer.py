921f284bda499a7f697e4588f52cab66998beba9
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-08-24 00:15:13 +0700

    add support live video classification

--- video_streamer.py
+++ video_streamer.py
@@ -16,23 +16,51 @@ def gen_frames():
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+counter = 0
+predict_every = 5
+preds = ''
+
 def gen_frames_processed():
+    global counter
     while True:
         success, frame = camera.read()
         if not success:
             break
-        else:
-            frame = frame_masking(frame)
-            frame = cropping(frame)
-            frame = lbp(frame)
-            # print(frame.shape)
-            # preds = model.predict(frame)
-            # preds = decode_predictions(preds, top=1)
-            # preds = str(preds[0,0,1])
-            # frame = cv2.putText(frame, 'OpenCV', (50, 50),
-            #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0),
-            #             2, cv2.LINE_AA)
+
+        counter += 1
+        frame = frame_masking(frame)
+        frame = lbp(frame)
+    
+        if counter == predict_every:
+            preds = np.zeros((287, 304, 3))
+            for _ in range(3):
+                preds[:, :, _] = frame[236:540, 280:567].T
+            preds = np.expand_dims(preds, axis=0)
+
+            preds = model.predict(preds)
+            preds = decode_predictions(preds, top=1)
+            preds = str(preds[0][0][1])
+            counter = 0
+
+            frame = cv2.putText(frame, 'Result: ' + preds, (50, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0),
+                    2, cv2.LINE_AA)
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # else:
+        #     frame = frame_masking(frame)
+        #     frame = cropping(frame)
+        #     frame = lbp(frame)
+        #     # print(frame.shape)
+        #     # preds = model.predict(frame)
+        #     # preds = decode_predictions(preds, top=1)
+        #     # preds = str(preds[0,0,1])
+        #     # frame = cv2.putText(frame, 'OpenCV', (50, 50),
+        #     #             cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0),
+        #     #             2, cv2.LINE_AA)
+        #     ret, buffer = cv2.imencode('.jpg', frame)
+        #     frame = buffer.tobytes()
+        #     yield (b'--frame\r\n'
+        #             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')

e5577d8444e7bbe2a3aecf35bc9233b4effd6d61
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-09-17 07:17:18 +0700

    revert to original

--- model_loader.py
+++ model_loader.py
@@ -4,7 +4,7 @@ from keras.models import load_model
 import tensorflow as tf
 import numpy as np
 import json
-
+from preprocessor import lbp
 model_path = 'model/lbp-model.h5'
 model = load_model(model_path)
 
@@ -15,6 +15,20 @@ def model_predict(img_path, model=model):
     x = preprocess_input(x)
     preds = model.predict(x)
     return preds
+
+# def roi_predict(img_path, model=model):
+#   img_ = image.load_img(img_path)
+#   img_ = lbp(img_)
+#   x_ = image.img_to_array(img_)
+#   x_ = np.expand_dims(x_, axis=0)
+#   preds = np.zeros((287, 304, 3))
+#   for _ in range(3):
+#     preds_[:, :, _] = x_[236:540, 280:567].T
+#     preds_ = np.expand_dims(preds_, axis=0)
+#     preds_ = model.predict(preds_)
+#     preds_ = decode_predictions(preds_, top=1)
+#     preds_ = str(preds_[0][0][1])
+#   return preds_
 
 def decode_predictions(preds, top=6, class_list_path='model/class.json'):
   if len(preds.shape) != 2 or preds.shape[1] != 6:
